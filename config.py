a2b2979eccf7dbe3ef4cc1e83cea81d97d81f721
AnasAyub-08 <anas.ayub0001@gmail.com>
2025-08-27 22:19:01 +0500

    migrating to postgres

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev_secret_key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///site.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
