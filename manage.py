253bf61d1acc4aa39a556e7ddd49f3e37ae8099c
AnasAyub-08 <anas.ayub0001@gmail.com>
2025-08-27 22:33:08 +0500

    more debugging

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask.cli import FlaskGroup
 from flask_migrate import Migrate
 from models import db
 from app import app   # import the app you already built
 
 migrate = Migrate(app, db)
+cli = FlaskGroup(app)
 
 if __name__ == "__main__":
-    app.run()
+    cli()
