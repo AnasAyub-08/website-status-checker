26054a050b15216111b62594b8803ea31c70037c
AnasAyub-08 <anas.ayub0001@gmail.com>
2025-08-27 22:39:12 +0500

    db fix

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev_secret_key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:GiFCrvKokTPcOCdHzsgaVLBFXfSZjcPO@postgres.railway.internal:5432/railway")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
